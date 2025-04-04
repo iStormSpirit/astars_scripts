be2f7b171a9b82702589125a74074a50afec7ac4
George <nowson1a@gmail.com>
2025-04-04 11:14:03 +0300

    add auto update for auto donate

--- Auto_refresh_and_donate.js
+++ Auto_refresh_and_donate.js
@@ -10,16 +10,18 @@
 // @match        https://as1.astars.club/clubs/6/boost/
 // @match        https://asstars.tv/clubs/6/boost/
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/iStormSpirit/astars_scripts/refs/heads/master/Auto_refresh_and_donate.js
+// @downloadURL  https://raw.githubusercontent.com/iStormSpirit/astars_scripts/refs/heads/master/Auto_refresh_and_donate.js
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
-    let isActivated = false; // Флаг активации скрипта
+    let isActivated = false;
 
     // Создаём кнопку для активации
     const button = document.createElement('button');
-    button.innerText = 'Start Auto Donate'; // Текст на кнопке
+    button.innerText = 'Start Auto Donate';
     button.style.position = 'fixed';
     button.style.top = '20px';
     button.style.left = '20px';
@@ -32,24 +34,21 @@
     button.style.fontSize = '16px';
     button.style.cursor = 'pointer';
 
-    // Добавляем кнопку на страницу
     document.body.appendChild(button);
 
-    // Обработчик нажатия на кнопку
     button.addEventListener('click', () => {
-        isActivated = !isActivated; // Переключаем флаг (вкл/выкл)
+        isActivated = !isActivated;
         if (isActivated) {
             console.log("Auto Donate Activated.");
             button.innerText = 'Stop Auto Donate';
-            button.style.backgroundColor = '#FF5733'; // Красный цвет (стоп)
+            button.style.backgroundColor = '#FF5733';
         } else {
             console.log("Auto Donate Deactivated.");
             button.innerText = 'Start Auto Donate';
-            button.style.backgroundColor = '#4CAF50'; // Зелёный цвет (старт)
+            button.style.backgroundColor = '#4CAF50';
         }
     });
 
-    // Функция для нажатия кнопки обновления карты
     function refreshCard() {
         let refreshButton = document.querySelector('button.club__boost__refresh-btn');
         if (refreshButton) {
@@ -58,7 +57,6 @@
         }
     }
 
-    // Функция для нажатия кнопки внесения карты
     function donateCard() {
         let donateButton = document.querySelector('button.club__boost-btn');
         if (donateButton) {
@@ -67,11 +65,10 @@
         }
     }
 
-    // Основной цикл (работает только при включении)
     setInterval(() => {
-        if (isActivated) { // Выполняем только если скрипт активирован
-            refreshCard(); // Обновляем карту
-            setTimeout(donateCard, 10); // Первое внесение через 10 мс
+        if (isActivated) {
+            refreshCard();
+            setTimeout(donateCard, 10);
         }
-    }, 10); // Интервал 100 мс (0.1 секунды)
+    }, 10);
 })();
